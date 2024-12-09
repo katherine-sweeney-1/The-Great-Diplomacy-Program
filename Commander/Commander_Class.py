1eb7d00300cb34640af7519e17e909e9949527dc
Katherine S <k.sweeney.not.important@gmail.com>
2024-12-09 13:43:54 -0500

    commander hard data, create functions and run files

--- Commander/Commander_Class.py
+++ Commander/Commander_Class.py
@@ -1,14 +1,24 @@
 
 class Commander ():
 
-    def __init__ (self, human, country, unit_members, dots):
+    """
+        human => string
+        country => string
+        unit members => list of unit objects?
+        own_dot_ters => list of territory objects?
+        occ_dot_ters => list of territory objects?
+    """
+
+    def __init__ (self, human, country, unit_members, owned_dots, occ_dots):
         self.human = human
         self.country = country
         self.unit_members = unit_members
-        self.dots = dots
+        self.own_dots = owned_dots
+        self.occ_dots = occ_dots
+
 
     def verify_commander (self, person_giving_orders):
         if  person_giving_orders == self.human:
-            return "Valid Commander"
+            return "Valid"
         else:
-            return "Invalid Commander"
+            return "Invalid"
