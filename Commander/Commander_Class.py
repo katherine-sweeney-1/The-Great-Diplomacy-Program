5de60e2d4d0beb63ee7564c155b1ca9253167a90
Katherine S <k.sweeney.not.important@gmail.com>
2024-12-12 12:37:24 -0500

    cleaned up commander class comments and code more

--- Commander/Commander_Class.py
+++ Commander/Commander_Class.py
@@ -1,39 +1,19 @@
 
 from Commander_Class_Helper_Functions import get_ters, check_node_printing
 
-
 class Commander ():
 
-    """
-        human => string
-        country => string
-        unit members => list of unit objects 
-            1. need to create unit objects
-            2. create commander class property to get the unit objects
-        own_dot_ters => list of territory objects
-        occ_dot_ters => list of territory objects
-    """
-
     def __init__ (self, human, country, unit_members, owned_dots, occ_ters):
-        self.human = human
-        self.country = country
-        self.unit_members = unit_members
-        self.own_dots = owned_dots
-        self.occ_ters = occ_ters
-
-    def get_ter_obj (self):
-        return "hello world"
-
-    def verify_commander (self, person_giving_orders):
-        if  person_giving_orders == self.human:
-            return "Valid"
-        else:
-            return "Invalid"
+        self.human = human                              # string 
+        self.country = country                          # string
+        self.unit_members = unit_members                # unit object
+        self.own_dots = owned_dots                      # node object
+        self.occ_ters = occ_ters                        # node object
     
-    def get_own_dots(self):
+    def get_own_dots(self):                             # retrieve node objects for dots owned
         own_dots = get_ters(self.own_dots)
         self.own_dots = own_dots
 
-    def get_occ_ters(self):
+    def get_occ_ters(self):                             # retrieve node objects for occupied territories
         occ_ters = get_ters(self.occ_ters)
         self.occ_ters = occ_ters
