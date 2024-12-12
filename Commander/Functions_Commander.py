0061466845c77f261005e72cd239f00ebde2b78b
Katherine S <k.sweeney.not.important@gmail.com>
2024-12-12 12:43:07 -0500

    adjusted output format

--- Commander/Functions_Commander.py
+++ Commander/Functions_Commander.py
@@ -12,12 +12,16 @@ def create_commander (indiv_key, nested_info):
 def check_class_works(cmdr_object):
     print("commander {} has dots in territories {}".
           format(cmdr_object.human, cmdr_object.own_dots))
-    print("commander {} occupies territories {}".format(cmdr_object.human, cmdr_object.occ_ters))
+    print("commander {} occupies territories {}".
+          format(cmdr_object.human, cmdr_object.occ_ters))
     print(" ")
 
 def run_create_commander (commander_data):
+    commander_list = []
     for each_key in commander_data:
         indiv_cmdr = create_commander(each_key, commander_data[each_key])
         indiv_cmdr.get_own_dots()
         indiv_cmdr.get_occ_ters()
-        check_class_works(indiv_cmdr)
+        commander_list.append(indiv_cmdr)
+    return commander_list
+        #check_class_works(indiv_cmdr)
--- Commander/Run_Commander_Class.py
+++ Commander/Run_Commander_Class.py
@@ -2,6 +2,9 @@
 from Commander_Hard_Data import Commander_Data_1 as data
 from Functions_Commander import run_create_commander
 
-run_create_commander(data)
-
-
+commanders = run_create_commander(data)
+"""
+print(commanders)                                       #check list of commanders is correct 
+for each_cmdr in commanders:
+    print(each_cmdr.human, each_cmdr.country)
+"""
