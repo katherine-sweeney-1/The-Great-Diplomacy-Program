3df3332e061ff1c87312d5d5aa37ba9d2275f814
Katherine S <k.sweeney.not.important@gmail.com>
2024-12-13 09:39:57 -0500

    made create commanders as one function

--- Commander/Functions_Commander.py
+++ Commander/Functions_Commander.py
@@ -1,6 +1,7 @@
 
 from Commander_Class import Commander
 
+"""
 def create_commander (indiv_key, nested_info):
     commander = Commander(indiv_key, 
                                 nested_info["Country"], 
@@ -13,6 +14,22 @@ def run_create_commander (commander_data):
     commander_list = []
     for each_key in commander_data:
         indiv_cmdr = create_commander(each_key, commander_data[each_key])
+        indiv_cmdr.get_own_dots()
+        indiv_cmdr.get_occ_ters()
+        # indiv_cmdr.check_class_works()
+        commander_list.append(indiv_cmdr)
+    return commander_list
+"""
+
+def create_commanders (commander_data):
+    commander_list = []
+    for each_key in commander_data:
+        nested_info = commander_data[each_key]
+        indiv_cmdr = Commander(each_key, 
+                                nested_info["Country"], 
+                                nested_info["Unit Members"],
+                                nested_info["Dots Owned"],
+                                nested_info["Dots Occ"])
         indiv_cmdr.get_own_dots()
         indiv_cmdr.get_occ_ters()
         # indiv_cmdr.check_class_works()
--- Commander/Run_Commander_Class.py
+++ Commander/Run_Commander_Class.py
@@ -1,11 +1,11 @@
 
 from Commander_Hard_Data import Commander_Data_1 as data
-from Functions_Commander import run_create_commander
+from Functions_Commander import create_commanders
 
-commanders = run_create_commander(data)
+commanders = create_commanders(data)
 
-"""
+
 print(commanders)                                       #check list of commanders is correct 
 for each_cmdr in commanders:
     print(each_cmdr.human, each_cmdr.country)
-"""
+
