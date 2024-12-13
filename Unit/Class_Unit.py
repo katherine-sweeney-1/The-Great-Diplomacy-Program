892119c47bf8be4c73f43594e00813d13608da4d
Katherine S <k.sweeney.not.important@gmail.com>
2024-12-13 09:50:23 -0500

    get unit location function and print function

--- Unit/Class_Unit.py
+++ Unit/Class_Unit.py
@@ -2,6 +2,8 @@ import sys
 import os
 sys.path.append(os.path.join("C:\\Users\\kathe\\Documents\\Py_Code\\Diplomacy\\Territories"))
 from Nodes_Class import Node
+from Nodes_Functions import run_dict_format
+data = "data/Nodes_No_Coords.csv"
 
 
 class Unit ():
@@ -13,8 +15,21 @@ class Unit ():
         self.command = command
 
     def get_loc (self):
-        print("location of unit obj", self.loc)
-        loc_node = Node(
-                        
+        nodes_dict = run_dict_format(data)
+        loc_data = nodes_dict[self.loc]
+        loc_node = Node ( self.loc,
+                         loc_data["Full Name"],
+                         loc_data["Type"],
+                         loc_data["Neighbors"],
+                         loc_data["Country"],
+                         loc_data["Dot"],
+                         loc_data["Home SupCenter"]
                         )
         self.loc = loc_node
+
+    def print_statements(self):
+        print(" ")
+        print("Unit ID", self.id)
+        print("Unit type", self.type)
+        print("Unit location", self.loc.name, self.loc)
+        print("Unit command", self.command)
--- Unit/Functions_Unit.py
+++ Unit/Functions_Unit.py
@@ -1,21 +1,16 @@
 
 from Class_Unit import Unit
 
-def create_unit(indiv_unit_name, nested_info):
-    indiv_unit = Unit(
-                        indiv_unit_name, 
+
+def create_units(units_data):
+    for each_unit in units_data:
+        nested_info = units_data[each_unit]
+        new_unit = Unit(
+                        each_unit,
                         nested_info["type"],
                         nested_info["loc"],
                         nested_info["command"]
-                    )
-    return indiv_unit
 
-def run_create_unit(units_data):
-    for each_unit in units_data:
-        new_unit = create_unit(each_unit, units_data[each_unit])
+        )
         new_unit.get_loc()
-        print(" ")
-        print("Unit ID", new_unit.id)
-        print("Unit type", new_unit.type)
-        print("Unit location", new_unit.loc)
-        print("Unit command", new_unit.command)
+        new_unit.print_statements()
