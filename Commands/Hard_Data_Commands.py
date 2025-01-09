0837a3f715902c39c7a49b17f439255b077d9f9f
Katherine S <k.sweeney.not.important@gmail.com>
2025-01-09 15:42:17 -0500

    transferring command class here

---
+++ Commands/Hard_Data_Commands.py
@@ -0,0 +1,96 @@
+cmds_data_1 = {
+    "UK01": {
+        "location" : "Lon",              # attack
+        "origin" : "Lon",
+        "destination" : "Nth",
+        "country": "UK",
+        "owner" : "Katherine"
+    },
+    "UK02": {                       # support
+        "location" : "Edi",
+        "origin" : "Lon",
+        "destination" : "Nth",
+        "country": "UK",
+        "owner" : "Katherine"
+    },
+    "UK03": {                       # attack  test for unit not in commander's members
+        "location" : "Lvp",
+        "origin" : "Lvp",
+        "destination" : "Lon",
+        "country": "UK",
+        "owner" : "Katherine"
+    },
+    "FR01": {                       # attack 
+        "location" : "Par",
+        "origin" : "Bre",
+        "destination" : "Bre",
+        "country" : "FR",
+        "owner" : "Mercy"
+    },
+    "FR02": {                       # attack
+        "location" : "Mar",                     
+        "origin" : "Mar",
+        "destination" : "Spa-SC",
+        "country" : "FR",
+        "owner" : "Mercy"
+    },
+    "FR03": {                       # hold
+        "location" : "Bre",
+        "origin" : "Bre",
+        "destination" : "Bre",
+        "country" : "FR",
+        "owner" : "Mercy"
+    },
+    "GE01": {                       # attack
+        "location" : "Kie",
+        "origin" : "Kie",
+        "destination" : "Hol",
+        "country" : "GE",
+        "owner" : "Kamran"
+    },
+    "GE02": {                       # attack
+        "location" : "Ber",
+        "origin" : "Ber",
+        "destination" : "Kie",
+        "country" : "GE",
+        "owner" : "Kamran"
+    },
+    "GE03": {                       # support
+        "location" : "Mun",
+        "origin" : "Ber",
+        "destination" : "Kie",
+        "country" : "GE",
+        "owner" : "Kamran"
+    },
+    "RU01": {                       # attack
+        "location" : "Stp-SC",
+        "origin" : "Stp-SC",
+        "destination" : "Bot",
+        "country" : "RU",
+        "owner" : "Michael"
+    },
+    "RU02": {                       # attack
+        "location" : "Mos",
+        "origin" : "Mos",
+        "destination" : "Ukr",
+        "country" : "AU",           # test for wrong country
+        "owner" : "Michael"
+    },
+    "RU03": {                       # support 
+        "location" : "War",
+        "origin" : "Mos",
+        "destination" : "Ukr",
+        "country" : "RU",
+        "owner" : "Adam"            # test for wrong owner
+    },
+    "RU04": {                       # hold
+        "location" : "Sev",
+        "origin" : "Sev",
+        "destination" : "Sev",
+        "country" : "RU",
+        "owner" : "Michael"
+    },
+
+}
+
+# test  unit is at the right/wrong location
