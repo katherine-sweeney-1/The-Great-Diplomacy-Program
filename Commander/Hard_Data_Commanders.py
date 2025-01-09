454d6f0db5686c2a8cf94ea1e46cfea22e57506f
Katherine S <k.sweeney.not.important@gmail.com>
2025-01-09 15:39:44 -0500

    updated commander data

--- Commander/Hard_Data_Commanders.py
+++ Commander/Hard_Data_Commanders.py
@@ -1,22 +1,22 @@
 
-Commander_Data_1 = {
+cmdrs_data_1 = {
     "Katherine" : {"Country": "UK", 
                    "Unit Members": ["UK01", "UK02", "UK03"],
-                   "Dots Owned": ["Lon", "Lvp", "Yor"]
+                   "Dots Owned": ["Lon", "Lvp", "Edi"]
                    },
 
     "Mercy" : {"Country": "FR", 
                    "Unit Members": ["FR01", "FR02", "FR03"],
-                   "Dots Owned": ["Par", "Mar", "Bur"]
+                   "Dots Owned": ["Par", "Mar", "Bre"]
                    },
 
     "Kamran" : {"Country": "GE", 
                    "Unit Members": ["GE01", "GE02", "GE03"],
-                   "Dots Owned": ["Mun", "Kie", "Den"]
+                   "Dots Owned": ["Mun", "Kie", "Ber"]
                    },
 
     "Michael" : {"Country": "RU", 
                    "Unit Members": ["RU01", "RU02", "RU03", "RU04"],
-                   "Dots Owned": ["War", "Sev", "Stp", "Mos"]
+                   "Dots Owned": ["War", "Sev", "Stp-SC", "Mos"]
                    }
 }
