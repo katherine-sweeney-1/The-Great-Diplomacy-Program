b02ff1e87452374f660263c0342a1dbbc0687fa4
katherine <katherine.m.sweeney@proton.me>
2025-08-06 16:11:49 -0400

    program and sql works

--- Tables/Class_Table.py
+++ Tables/Class_Table.py
@@ -11,13 +11,12 @@ class Table ():
     )
 
     def create_table(self):
-            #self.db = mysql_connect()
-            #self.db.query("""SHOW DATABASES;""")
             self.db.query("""
             USE tgdp_1;
             """)
+            self.db.store_result()
             self.db.query("""
-                CREATE TABLE IF NOT EXISTS game1_1901.5 (
+                CREATE TABLE IF NOT EXISTS game1_1901_fall (
                 UNIT_ID TEXT,
                 Commander TEXT,
                 Location TEXT,
@@ -34,14 +33,14 @@ class Table ():
         sql = """USE tgdp_1;"""
         self.db.query(sql)
         sql = """
-            DELETE FROM game1_{};
-            """.format(self.turn_string)
+            DELETE FROM game1_1901_fall;
+            """
         self.db.query(sql)
         for cmd_string in cmds:
             cmd = cmds[cmd_string]
             sql = """
-                INSERT INTO game1_{} (UNIT_ID, Commander, Location, Origin, Destination, Outcome) VALUES ("{}", "{}", "{}", "{}", "{}", "{}")
+                INSERT INTO game1_1901_fall (UNIT_ID, Commander, Location, Origin, Destination, Outcome) VALUES ("{}", "{}", "{}", "{}", "{}", "{}")
                 ON DUPLICATE KEY UPDATE UNIT_ID = UNIT_ID;
-                """.format(self.turn_string, cmd.unit.id, cmd.human.human, cmd.loc.name, cmd.origin.name, cmd.destination.name, cmd.outcome_loc.name)
+                """.format(cmd.unit.id, cmd.human.human, cmd.loc.name, cmd.origin.name, cmd.destination.name, cmd.outcome_loc.name)
             self.db.query(sql)
         self.db.store_result()
