54f7896cde09abecf188ea9ee44c2f0fe1eafebe
katherine-sweeney-1 <katherine.m.sweeney@proton.me>
2025-11-09 12:58:10 -0500

    begin testing for game 3

---
+++ data/Txt_Hard_Data/Cmdrs_3.py
@@ -0,0 +1,31 @@
+cmdrs_3_1901 = {
+    "Adam" : {"Country": "AU", 
+                   "Unit Members": ["AU01", "AU02", "AU03"],
+                   "Dots Owned": ["Bud", "Vie", "Tri"]
+                   },
+    "Liam" : {"Country": "UK", 
+                   "Unit Members": ["UK01", "UK02", "UK03"],
+                   "Dots Owned": ["Edi", "Lon", "Lvp"]
+                   },
+    "Mercy" : {"Country": "FR",
+                    "Unit Members": ["FR01", "FR02", "FR03"],
+                    "Dots Owned": ["Par", "Mar", "Bre"]
+                    },
+    "Kamran" : {"Country": "GE", 
+                   "Unit Members": ["GE01", "GE02", "GE03"],
+                   "Dots Owned": ["Mun", "Kie", "Ber"]
+                   },
+
+    "Michael" : {"Country": "IT", 
+                   "Unit Members": ["IT01", "IT02", "IT03"],
+                   "Dots Owned": ["Rom", "Nap", "Ven"]
+                   },
+    "Katherine" : {"Country": "RU", 
+                   "Unit Members": ["RU01", "RU02", "RU03", "RU04"],
+                   "Dots Owned": ["Sev", "Stp-SC", "War", "Mos"]
+                   },
+    "Will" : {"Country": "TU", 
+                   "Unit Members": ["TU01", "TU02", "TU03"],
+                   "Dots Owned": ["Smy", "Ank", "Con"]
+                   }
+}
