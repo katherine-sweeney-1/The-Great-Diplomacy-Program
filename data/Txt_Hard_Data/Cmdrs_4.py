00a5a94c52604fef63424a74dc5701d55c7b2698
katherine-sweeney-1 <katherine.m.sweeney@proton.me>
2025-12-02 09:39:12 -0500

    game 4 1901 works

---
+++ data/Txt_Hard_Data/Cmdrs_4.py
@@ -0,0 +1,31 @@
+cmdrs_4_1901 = {
+    "Zohaib" : {"Country": "AU", 
+                   "Unit Members": ["AU01", "AU02", "AU03"],
+                   "Dots Owned": ["Bud", "Vie", "Tri"]
+                   },
+    "Michael" : {"Country": "UK", 
+                   "Unit Members": ["UK01", "UK02", "UK03"],
+                   "Dots Owned": ["Edi", "Lon", "Lvp"]
+                   },
+    "Keaton" : {"Country": "FR",
+                    "Unit Members": ["FR01", "FR02", "FR03"],
+                    "Dots Owned": ["Par", "Mar", "Bre"]
+                    },
+    "Will" : {"Country": "GE", 
+                   "Unit Members": ["GE01", "GE02", "GE03"],
+                   "Dots Owned": ["Mun", "Kie", "Ber"]
+                   },
+
+    "Liam" : {"Country": "IT", 
+                   "Unit Members": ["IT01", "IT02", "IT03"],
+                   "Dots Owned": ["Rom", "Nap", "Ven"]
+                   },
+    "Kamran" : {"Country": "RU", 
+                   "Unit Members": ["RU01", "RU02", "RU03", "RU04"],
+                   "Dots Owned": ["Sev", "Stp-SC", "War", "Mos"]
+                   },
+    "Katherine" : {"Country": "TU", 
+                   "Unit Members": ["TU01", "TU02", "TU03"],
+                   "Dots Owned": ["Smy", "Ank", "Con"]
+                   }
+}
